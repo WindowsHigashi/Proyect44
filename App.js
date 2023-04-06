f35aad0cecfa918ab8f06888f5b6dd499b2c3f69
WindowsHigashi <111473567+WindowsHigashi@users.noreply.github.com>
2023-04-05 19:58:49 -0600

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import { StyleSheet, View, Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 
 export default class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      text: ''
+    }
+  }
   render(){
     return(
       <View style={{flex:1}}>
